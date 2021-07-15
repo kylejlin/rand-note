e34bacfd32a06e3dc0b46536f2b9d094372a6f3c
Kyle Lin <kylejlin.us@gmail.com>
2021-07-15 13:09:35 -0700

    Update scripts

--- scripts/smart_install.sh
+++ scripts/smart_install.sh
@@ -2,10 +2,7 @@
     tar -xzf node_modules_backup/node_modules.tar.gz &&
     cmp package.json node_modules_backup/package_json_checksum.json &&
     echo "Installed successfully from backup"
-)
-
-||
-
+) ||
 (
     echo "Could not install from backup. Falling back to manual install..." &&
     npm install &&
